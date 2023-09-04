f864e83086dd4ecabe854f0c6f03967521442a7a
Prof. Ricardo Leme <ricardo.leme@fatecitu.edu.br>
2023-09-04 12:31:23 -0300

    public folder

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const app = express()
+const port = 4000 
+
+app.use(express.json()) // irá fazer o parse de arquivos JSON
+//Rotas de conteúdo público
+app.use('/', express.static('public'))
+
+//Configura o favicon
+app.use('/favicon.ico', express.static('public/images/computer.png'))
+
+//Rotas da API
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        message: 'API Fatec 100% funcional🖐',
+        version: '1.0.0'
+    })
+})
+//Rotas de Exceção - deve ser a última!
+app.use(function (req, res) {
+    res.status(404).json({
+        errors: [{
+            value: `${req.originalUrl}`,
+            msg: `A rota ${req.originalUrl} não existe nesta API!`,
+            param: 'invalid route'
+        }]
+    })
+})
+app.listen(port, function(){
+    console.log(`🖥 Servidor rodando na porta ${port}`)
+})
