d3999db9695911c8d569b5f7ecf050bd6857150c
Prof. Ricardo Leme <ricardo.leme@fatecitu.edu.br>
2023-10-16 12:50:23 -0300

    JWT Aula 16/10

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 4000 
 //import das rotas da app
 import rotasPrestadores from './routes/prestador.js'
+import rotasUsuarios from './routes/usuario.js'
 
 //app.use(express.urlencoded({ extended: true}))
 app.use(express.json()) // irá fazer o parse de arquivos JSON
@@ -15,6 +16,7 @@ app.use('/favicon.ico', express.static('public/images/computer.png'))
 
 //Rotas da API
 app.use('/api/prestadores', rotasPrestadores)
+app.use('/api/usuarios', rotasUsuarios)
 
 app.get('/api', (req, res) => {
     res.status(200).json({
